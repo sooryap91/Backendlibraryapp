75dc54f1afacfc904b1835655a38d3fe0c86fa85
sooryap91 <112260050+sooryap91@users.noreply.github.com>
2022-11-25 18:31:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = new express();
+const DATA  = require('./models/book')
+
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 const cors = require('cors')
